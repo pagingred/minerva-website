3b1c98c75ad53b83f588dbb37357bb0297d1f0aa
Nicholas Johnson <nrj@minervadefense.com>
2026-04-15 15:42:06 -0400

    Add back partner categories.

--- app/data/partners.ts
+++ app/data/partners.ts
@@ -2,18 +2,22 @@
 export const partners = [
   {
     title: "The U.S. Army Redstone Test Center",
+    category: "Software | Engineering",
     image: "/rtc.jpg",
   },
   {
     title: "The U.S. Army Space and Missile Defense Command",
+    category: "Software | Engineering",
     image: "/usasmdc.webp",
   },
   {
     title: "The Ronald Reagan Space and Missile Test Range",
+    category: "Software | Engineering",
     image: "/rrbmdts.webp",
   },
   {
     title: "The Office of the Secretary of War Test Resources Management Center",
+    category: "Software | Engineering",
     image: "/dowtrmc.png",
   },
 ];
