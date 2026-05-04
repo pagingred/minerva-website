608317b0800833d54f0242ea49e678b53e98aa8c
Nicholas Johnson <nrj@minervadefense.com>
2026-05-04 14:43:08 -0400

    Make our partner cards redirect to their sites.

--- app/data/partners.ts
+++ app/data/partners.ts
@@ -3,17 +3,21 @@ export const partners = [
   {
     title: "The U.S. Army Redstone Test Center",
     image: "/us-army-rtc.png",
+	url: "https://atec.army.mil/rtc-home/"
   },
   {
     title: "The U.S. Army Space and Missile Defense Command",
     image: "/us-army-smdc.png",
+	url: "https://www.army.mil/smdc"
   },
   {
     title: "The Ronald Reagan Space and Missile Test Range",
     image: "/ronald-reagan-str.png",
+	url: "https://www.smdc.army.mil/ORGANIZATION/TC/"
   },
   {
     title: "Test Resource Management Center",
     image: "/dowtrmc.png",
+	url: "https://www.trmc.osd.mil/"
   },
 ];
