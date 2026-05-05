1b48e4f740ea32cf507f8de0ebb6d20e76025291
Nicholas Johnson <nrj@minervadefense.com>
2026-05-05 13:02:45 -0400

    Make capability cards wider.

--- app/components/CapabilityGrid.tsx
+++ app/components/CapabilityGrid.tsx
@@ -9,8 +9,8 @@ export default function CapabilitiesGrid() {
         grid gap-6
         grid-cols-1
         sm:grid-cols-2
-        md:grid-cols-3
-        lg:grid-cols-4
+        md:grid-cols-2
+        lg:grid-cols-3
       ">
         {capabilities.map((cap, i) => (
           <CapabilityCard key={i} {...cap} />
